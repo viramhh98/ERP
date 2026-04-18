9f05957e445c6c2c53b38f4cc79f890772ef8118
Viram Shah <shahviramhh98@gmail.com>
2026-04-18 16:48:43 +0530

    feat: add login API with JWT authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,21 @@
 //user routes
 const express=require('express');
 const router=express.Router();
-const signup=require('../controllers/signup.controller.js');
-const validateSignup=require('../middleware/validateSignup.middleware.js');
 
 
 
 // Create a new user
+const signup=require('../controllers/signup.controller.js');
+const validateSignup=require('../middleware/validateSignup.middleware.js');
 router.post('/signup',validateSignup,signup);
+
+
+
+// Login user
+const validateLogin=require('../middleware/validateLogin.middleware.js');
+const login=require('../controllers/login.controller.js');
+router.post('/login',validateLogin,login);
+
 
 
 module.exports=router;
